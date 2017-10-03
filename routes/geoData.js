0fceff48c414db904d9cbd4a84bffec16b49879f
Nalipp <nateplipp@gmail.com>
2017-10-03 12:00:48 -0700

    initial commit

---
+++ routes/geoData.js
@@ -0,0 +1,36 @@
+const request = require('request');
+require('dotenv').config();
+const apiKey = process.env.GOOGLE_API_KEY;
+
+module.exports = {
+  getData(address, cb) {
+    const geoCodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + apiKey
+    
+    request(geoCodeUrl, (error, response, body) => {
+      let result = { 
+        geoData: JSON.parse(body).results[0]
+      }
+      cb(result);
+    });
+  },
+
+  getRandomizedUrls(coordinates, urlCount=100, cb) {
+    let urlsArr = [];
+    let lat = String(coordinates.lat);
+    let lng = String(coordinates.lng);
+
+    let baseLat = lat.slice(0, lat.indexOf('.') + 3);
+    let baseLng = lng.slice(0, lng.indexOf('.') + 3);
+
+    console.log(baseLat);
+    console.log(baseLng);
+
+    for (let i = 0; i < urlCount; i++) {
+      let lat = baseLat + Math.floor(Math.random() * (999 - 100) + 100);
+      let lng = baseLng + Math.floor(Math.random() * (999 - 100) + 100);
+
+      urlsArr.push('https://maps.googleapis.com/maps/api/streetview?size=280x160&location=' + lat + ',' + lng + '&heading=151.78&pitch=-0.76&key=AIzaSyB-csukfcDPdI-2zc5geKecHwwl3atroA0');
+    }
+    cb(urlsArr);
+  }
+}
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const geoData = require('./geoData');
+
+/* GET home page. */
+router.get('/search', function(req, res, next) {
+  console.log('req.query', req.query);
+  let address = req.query.address;
+  let urlCount = req.query.urlCount;
+  console.log(req.query);
+
+  console.log('address', address);
+  console.log('urlCount', urlCount);
+
+  geoData.getData(address, result => {
+    let data = {
+      address: result.geoData.formatted_address,
+      lat: result.geoData.geometry.location.lat,
+      lng: result.geoData.geometry.location.lng
+    }
+
+    geoData.getRandomizedUrls(data, urlCount, resultUrls => {
+      data['urls'] = resultUrls;
+
+      console.log('11111111111111', data); 
+
+      res.render('index', {data: data}); 
+    });
+  });
+
+
+  
+});
+
+module.exports = router;
+
